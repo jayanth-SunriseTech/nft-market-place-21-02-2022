2c359eca66e7bf3a8a8f066b955135b7d91769a8
jayanth-SunriseTech <97939524+jayanth-SunriseTech@users.noreply.github.com>
2022-03-07 12:19:28 +0530

    NFT Card Button Fixed

--- src/components/NFTCard/NFTCard.js
+++ src/components/NFTCard/NFTCard.js
@@ -3,6 +3,10 @@ import nft from '../../Assets/nft.png';
 import "./NFTCard.css";
 
 function NFTCard({nftImage,price}) {
+
+  const handleClick = () => {
+    alert("hi");
+  }
   return (
     <div className='col-6 col-md-4 col-lg-3'>
     <div className='nftContainer'>
@@ -10,9 +14,9 @@ function NFTCard({nftImage,price}) {
   <div className="NFT_card_body" >
   <img src={nftImage} className="nftCard" />
   <span class="price_container">{price}</span>
-  <button class="bid_btn">
-    <span className='pc_bid'>Place a Bid</span>
-    <span className='mobile_bid'>Bid</span>
+  <button type='button' onClick={handleClick} className="bid_btn pc_bid">Place a Bid
+    </button> 
+  <button type='button' onClick={handleClick} className="bid_btn mobile_bid"> Bid
     </button> 
     
   </div>
